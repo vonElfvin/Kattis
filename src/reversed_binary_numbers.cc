263eebc7a8e248bc4a68ae4e5c5d1f318268bf6d
Elvin Granat <elvgr805@student.liu.se>
2017-11-03 22:21:12 +0100

    Done with Reversed Binary Numbers

--- src/reversed_binary_numbers.cc
+++ src/reversed_binary_numbers.cc
@@ -1,5 +1,21 @@
 #include <iostream>
+#include <bitset>
+#include <string>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
 
 int main(){
-    
+    int x{};
+    cin >> x;
+    bitset<30> const bit(x);
+    string s{bit.to_string()};
+    int const count = 30-s.find('1');
+    reverse(s.begin(), s.end());
+    int result{};
+    for(int i{0}; i<count; ++i){
+        result += pow(2, count-i-1)*(static_cast<int>(s.at(i))-48);
+    }
+    cout << result << endl;
 }
