407f3bd98ff5aa13ad79279abdaed435fa9ace6b
vonElfvin <elvin.granat@gmail.com>
2017-11-07 23:58:39 +0100

    Done with A Real Challenge

---
+++ src/a_real_challenge.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+    double a{};
+    cin >> a;
+    cout << fixed << setprecision(15) << sqrt(a)*4 << endl;
+}
